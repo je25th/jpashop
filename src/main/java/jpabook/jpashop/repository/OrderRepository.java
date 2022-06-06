749c449955c2e206bab65ddf41b514ac54006a19
je25th <je25th@kakao.com>
2022-06-06 23:13:10 +0900

    주문 조회 ~V6 : DTO 직접 조회 최적화

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -108,6 +108,16 @@ public class OrderRepository {
         ).getResultList();
     }
 
+    public List<Order> findAllWithMemberDelivery(int offset, int limit) {
+        return em.createQuery(
+                "select o from Order o" +
+                        " join fetch o.member m" +
+                        " join fetch o.delivery d", Order.class)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
+    }
+
     public List<Order> findAllWithItem() {
         //JPA의 distinct는
         // 1. SQL에 distinct를 추가하고,
